138916751639eb68ce4d1b93356f07217000adb8
dooooongyuan <128032721+dooooongyuan@users.noreply.github.com>
2026-07-30 18:34:18 +0800

    chore: use custom Wiki domain

--- wiki/astro.config.mjs
+++ wiki/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://xiayinglu-wiki.dongjingyuan282.workers.dev',
+  site: process.env.SITE_URL || 'https://xylwiki.cc.cd',
   output: 'static',
   devToolbar: {
     enabled: false,
